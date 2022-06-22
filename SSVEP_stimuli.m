53f1c6736167af4e203b2bacfecfca340eec95aa
Hong Gi Yeom <47119476+honggi82@users.noreply.github.com>
2022-06-22 17:07:59 +0900

    Update SSVEP_stimuli.m
    I changed the code for the rapid process by using the "timer" function.

--- SSVEP_stimuli.m
+++ SSVEP_stimuli.m
@@ -2,72 +2,47 @@
 % programed by Hong Gi Yeom at Chosun University 2022.06.03
 % Inputs of the function should be stimulus frequencies, and positions
 % ex)
-% stim_Hz=[5 6 7 8]; % Frequencies of the stimuli, Unit is Hz
+% stim_Hz=[5 6 7 8]; % Frequencies of the stimuli. Unit is Hz
 % stim_pos=[0.4, 0.1; 0.7 0.4; 0.4 0.7; 0.1 0.4]; % Positions of the stimuli
+% ex_time = 10; % execution time. Unit is second
 % % the values of the stim_pos range from 0 to 1
-% run "SSVEP_stimuli(stim_Hz, stim_pos);"
+% run "SSVEP_stimuli(stim_Hz, stim_pos, ex_time);"
 
-function SSVEP_stimuli(stim_Hz, stim_pos)
+function SSVEP_stimuli(stim_Hz, stim_pos, ex_time)
 
-width=0.2;            % Width of the stimuli
-height=0.2;           % Height of the stimuli
-refresh=60;           % Refresh rate of the computer monitor 
-resolution=[800 800]; % Resolution of the stimulus screen 
+width=0.2; % width of the stimuli
+height=0.2; % height of the stimuli
+refresh=60; % refresh rate of the computer monitor 
 
 % Stimulus signal generation
 t=1/refresh:1/refresh:1;
 for s=1:length(stim_Hz)
-    sig(s,:)=round((sin(2*pi*stim_Hz(s)*t)+1)/2);
+    sig(s,:)=round((sin(2*pi*stim_Hz(s)*t)+1)/2)+1;
 end
+%figure;for s=1:length(stim_Hz);subplot(length(stim_Hz),1,s); plot(sig(s,:));end
 
 % Pattern of the stimulus
 pattern(1,:,:)=[0,1,0,1; 
-                1,0,1,0;
-                0,1,0,1; 
-                1,0,1,0];
+        1,0,1,0;
+        0,1,0,1; 
+        1,0,1,0];
 pattern(2,:,:)=~pattern(1,:,:);
 
-stim=zeros(100);
 % Subplot positions of the stimuli
 for s=1:size(stim_pos,1)
-    x_st(s)=round(100*stim_pos(s,1));           % start position of x for the s-th stimulus
-    x_end(s)=round(100*(stim_pos(s,1)+width));  % end position of x for the s-th stimulus
-    y_st(s)=round(100*stim_pos(s,2));           % start position of y for the s-th stimulus
-    y_end(s)=round(100*(stim_pos(s,2)+height)); % end position of y for the s-th stimulus
+    pos(s,:)=[stim_pos(s,1), 1-stim_pos(s,2)-height, width, height];
 end
 
-% Upscaling the pattern 
-pattern_up(1,:,:)=kron(squeeze(pattern(1,:,:)), ones([round((x_end(1)-x_st(1)+1)/size(pattern,2)) round((y_end(1)-y_st(1)+1)/size(pattern,3))]));
-pattern_up(2,:,:)=kron(squeeze(pattern(2,:,:)), ones([round((x_end(1)-x_st(1)+1)/size(pattern,2)) round((y_end(1)-y_st(1)+1)/size(pattern,3))]));
-sz_pt= size(pattern_up);
-
-figure('Position',[0 0 resolution(1) resolution(2)]); set(gcf,'Color','k'); colormap('gray');
-t=1;
-
-while (1)
-    
-    tic;
-    % Generating the stimulus image
-    for s=1:size(stim_pos,1)
-        if sig(s,t)==0
-            stim(y_st(s):y_st(s)+sz_pt(3)-1,x_st(s):x_st(s)+sz_pt(2)-1)=pattern_up(1,:,:);
-        else
-            stim(y_st(s):y_st(s)+sz_pt(3)-1,x_st(s):x_st(s)+sz_pt(2)-1)=pattern_up(2,:,:);
-        end
-    end
-    
-    % Plotting the stimulus
-    imagesc(stim);
+figure; set(gcf,'Color','k'); colormap('gray');
+for s=1:size(pos,1)
+    subplot('Position', pos(s,:));
+    sp(s)=imagesc(squeeze(pattern(sig(s,1),:,:)));
     set(gca,'xtick',[],'ytick',[]); 
-    if t<refresh
-        t=t+1;
-    else
-        t=1;
-    end
-    
-    % update the figures
-    drawnow 
-    del=toc;
-    
-    pause(1/refresh-del);
 end
+
+% using a timer
+tm = timer('TimerFcn', 'Plot_SSVEP_stimuli(pattern, sig, sp, t); t=t+1; if t>refresh;t=1;end ');
+tm.Period = 1/refresh;
+tm.ExecutionMode = 'fixedRate';
+tm.TasksToExecute = refresh*ex_time;
+start(tm)
